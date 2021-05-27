a8f1b0f418b965c1f02bea74dcdb81b17c771fab
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-05-27 18:28:55 +0600

    Completed task

--- app/Models/Property_images.php
+++ app/Models/Property_images.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Property_images extends Model
 {
     public $table = 'property_images';
+
+    public $fillable = [
+        'house_id',
+        'user_id',
+        'img_name',
+        'primary_image',
+        'image_caption',
+        'created_at',
+        'updated_at'
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\UserBasicDetails;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,10 +14,37 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+/**
+ * 1. Check the form on the "form-validations" URL. After submitting this form, data should be inserted into the user_basic_details
+ * table with server-side Laravel validation rules as mentioned below:
+ *
+2. Full Name: Required, min length 4, Only Alphabets, space, and hyphen are allowed
+3. Phone number: Optional but if entered then only 10 digits number should be accepted
+4. Date of birth: Required, Difference between entered date and today must be more than 15 years
+
+ * dob more than 50
+ */
+
+Route::post('/test', function (Request $request) {
+    $dt = new Carbon\Carbon();
+    $before = $dt->subYears(50)->format('Y-m-d');
+
+    $request->validate([
+        'full_name' =>  'required|string',
+        'phone_number'  =>  'required|string',
+        'email' =>  'required|email',
+//        'dob'   =>  'required|string',
+        'dob' => 'required|date|before:' . $before
+    ]);
+
+    UserBasicDetails::create($request->all());
+    return back();
+})->name('storeData');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/property-details', 'Property_details@index'); 
-Route::get('/form-validation', 'Property_details@form_validation'); 
+
+Route::get('/property-details', 'Property_details@index');
+Route::get('/form-validation', 'Property_details@form_validation');
